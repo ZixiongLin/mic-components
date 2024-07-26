55287026a98ca7a41d64d97c854e9c4479aefe8a
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-07-26 20:41:42 +0100

    Fixing test Progress and TextArea component

--- app/components/Progress/__test__/test.spec.tsx
+++ app/components/Progress/__test__/test.spec.tsx
@@ -3,15 +3,18 @@ import { describe, expect, it } from "vitest";
 import { Progress } from "../Progress";
 
 describe("Progress bar", ()=> {
-    render(<Progress data-testid="test" min={10} max={110} value={20}
-        status="warning"
-    />)
+
     it("render", () => {
+        render(<Progress data-testid="test" min={10} max={110} value={20}
+            status="warning"
+        />)
         expect(screen.getAllByTestId("test")).not.toBeNull()
     })
     it("should have 20% progress bar", () =>{
-        expect(screen.queryByRole("status-bar")?.style.width).toBe("20%")
-    })
 
-    
+        render(<Progress data-testid="test" min={10} max={110} value={20}
+            status="warning"
+        />)
+        expect(screen.queryByRole("status-bar")?.style.width).toBe("20%")
+    })    
 })
