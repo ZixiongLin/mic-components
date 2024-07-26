502ed614e4424b4a9fce2120a82e3f351032ae05
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-07-26 23:29:39 +0100

    Started Layout pages

--- app/components/Badge/cva.ts
+++ app/components/Badge/cva.ts
@@ -3,12 +3,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 export const cvaBadge = cva("badget",{
     variants: {
         variant:{
-            success: ["bg-success-100 text-succes-300 border border-succes-300 m-2 p-1 rounded-full font-medium	uppercase"],
-            removed: ["bg-error-100 text-error-300 border border-error-300 m-2 p-1 rounded-full font-medium	uppercase"],
-            progress:["bg-primary-100 text-primary-300 border border-primary-300 m-2 p-1 rounded-full font-medium	uppercase"],
-            moved:["bg-warning-100 text-warning-300 border border-warning-300 m-2 p-1 rounded-full font-medium	uppercase"],
-            beta:["bg-secondary-100 text-secondary-300 border border-secondary-300 m-2 p-1 rounded-full font-medium	uppercase"],
-            trial:["bg-neutrals-100 text-neutrals-300 border border-neutrals-300 m-2 p-1 rounded-full font-medium	uppercase"],
+            success: ["bg-success-100 text-succes-300 border border-succes-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
+            removed: ["bg-error-100 text-error-300 border border-error-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
+            progress:["bg-primary-100 text-primary-300 border border-primary-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
+            moved:["bg-warning-100 text-warning-300 border border-warning-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
+            beta:["bg-secondary-100 text-secondary-300 border border-secondary-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
+            trial:["bg-neutrals-100 text-neutrals-300 border border-neutrals-300 m-2 py-2 px-4 rounded-full font-medium	uppercase"],
         }
       },
       compoundVariants: [{variant: "success" }],
--- app/pages/components/layout.tsx
+++ app/pages/components/layout.tsx
@@ -1,8 +1,44 @@
+import { Badge } from "@/app/components/Badge";
+import { Link } from "@/app/components/Link";
+import { ScrollArea } from "@/app/components/ScrollArea";
 import { ReactNode } from "react";
 
 const Layout = ({children}:{children:ReactNode}) => {
-    return <div>
-        {children}
+    return <div className="flex">
+        <ScrollArea className="w-3/12 h-full max-h-screen min-h-screen">
+            <div className="flex flex-col">
+                <Link href="#">
+                    Badge <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Button <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Check <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Input <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Link <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Pill <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Progress <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    Separator <Badge variant="success">new</Badge>
+                </Link>
+                <Link href="#">
+                    TextArea <Badge variant="success">new</Badge>
+                </Link>
+            </div>
+        </ScrollArea>
+        <section>
+            {children}
+        </section>
     </div>
 }
 
