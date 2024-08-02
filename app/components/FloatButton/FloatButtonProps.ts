30dd3570ae756fdede8948d932c983e8dd6cb31f
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-08-02 16:41:36 +0100

    Float Button done

---
+++ app/components/FloatButton/FloatButtonProps.ts
@@ -0,0 +1,5 @@
+import { ButtonHTMLAttributes } from "react";
+
+export interface FloatButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { Autocomplete } from './components/Autocomplete/Autocomplete'
 import { Avatar } from './components/Avatar'
 import { Check } from './components/Check'
 import { ColorPicker } from './components/ColorPicker'
+import { FloatButton } from './components/FloatButton'
 import { Input } from './components/Input'
 import { Link } from './components/Link'
 import { Progress } from './components/Progress'
@@ -88,6 +89,13 @@ export default function Home (): JSX.Element {
 
       <h1>Snippet</h1>
       <Snippet>npm i @chinese/button</Snippet>
+      
+      <h1>Float Button</h1>
+      <FloatButton>
+        <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="#ffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M16 10.5h.01m-4.01 0h.01M8 10.5h.01M5 5h14a1 1 0 0 1 1 1v9a1 1 0 0 1-1 1h-6.6a1 1 0 0 0-.69.275l-2.866 2.723A.5.5 0 0 1 8 18.635V17a1 1 0 0 0-1-1H5a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1Z"/>
+        </svg>
+      </FloatButton>
     </main>
   )
 }
