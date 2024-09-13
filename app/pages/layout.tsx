f920d28493e2945c19db5d98d9cc1a49d06400ce
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2024-09-13 12:17:32 +0100

    Fixed Styles

--- app/pages/layout.tsx
+++ app/pages/layout.tsx
@@ -4,18 +4,18 @@ import { Navbar } from '../components/Nav/Nav'
 
 const Layout = ({ children }: { children: ReactNode }): JSX.Element => {
   return (
-    <>
-      <Navbar></Navbar>
-      <div className='flex w-full'>
-        <div className='w-1/5'>
-          <SideMenu />
+      <div className='bg-gradient-to-b from-success-200 to-warning-100'>
+        <Navbar></Navbar>
+        <div className="flex w-full">
+          <div className='w-1/5'>
+            <SideMenu />
+          </div>
+          <section className='w-4/5 h-full px-4'>
+            {children}
+          </section>
         </div>
-        <section className='w-4/5 h-full px-4'>
-          {children}
-        </section>
-      </div>
-    </>
 
+      </div>
   )
 }
 
